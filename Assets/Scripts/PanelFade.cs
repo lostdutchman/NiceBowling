86de5608d9d48c663cba74c833919d053fabe1f1
Sam <Sam@MAGICKINGDOM>
2018-01-05 10:20:34 -0700

    put splash screen into opening scene
    panel fade script, put items into opening scene

---
+++ Assets/Scripts/PanelFade.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelFade : MonoBehaviour {
+
+    public Image Splash;
+    float fadeTime = 3f;
+    Color colorToFadeTo;
+
+	// Use this for initialization
+	void Start () {
+        colorToFadeTo = new Color(1f, 1f, 1f, 0f);
+        Splash.CrossFadeColor(colorToFadeTo, fadeTime, true, true);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
