93296c5cfe6f58b7a15fad83e2f550764aeed3a0
Sam <Sam@MAGICKINGDOM>
2018-01-05 09:47:12 -0700

    Scripted Splash Screen
    Added float to levelmanager, didn't work, undid changes, made new script specifically for splash screen, tested

---
+++ Assets/Scripts/SplashLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashLoader : MonoBehaviour
+{
+    public float delay = 4;
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));   
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Application.LoadLevel("Opening");
+    }
+}
