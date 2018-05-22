bd6822529d2692040376a1db0785fd7d300f34c1
Sam <Sam@MAGICKINGDOM>
2018-05-22 16:48:59 -0700

    imported windmill and roomba

---
+++ Assets/Scripts/RoombaMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoombaMove : MonoBehaviour {
+
+    public float speed = 10f;
+    public float forwardSpeed = 2f;
+    Rigidbody body;
+
+    void Start()
+    {
+        body = GetComponentInChildren<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //body.AddForce(transform.forward * forwardSpeed);
+            transform.position += transform.forward * Time.deltaTime * forwardSpeed;
+        }
+        else
+        {
+            transform.Rotate(Vector3.up, speed * Time.deltaTime);
+        }
+	
+	}
+}
